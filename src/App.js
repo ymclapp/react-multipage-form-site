4bfd3e70d18e33f120f3f192f57681b1e3ce1192
ymcla <ymclapp@yahoo.com>
2022-01-19 09:32:23 -0600

    added partials and they are working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavMenu from './components/partials/NavMenu';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
 
 import Home from './components/Home';
 
 function App() {
   return (
     <>
+    <NavMenu />
+    <Header />
+
     <Switch>
+
       <Route exact path={['/', '/home']}>
         <Home />
       </Route>
+
+
     </Switch>
+
+    <Footer />
+
     </>
   );
 }
